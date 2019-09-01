cc70617f5310ade610728f43684e62c4d4947f99
Natalie-Rodriguez <nataliediazrodriguez@gmail.com>
2019-09-01 13:05:14 -0400

    add sign up vue/about vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ message }}</h1>
+    <p>{{ body }}</p>
   </div>
 </template>
+
+<style></style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "About us",
+      body: "Add some text here when you are done with MVP Stuff  "
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
